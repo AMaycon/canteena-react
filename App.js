947e520b497eca2f2ca2cba9159fd6af2cf8a676
rafael conrado <rafael070893@hotmail.com>
2020-10-05 23:21:08 -0300

    rotas funcionando

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
+import 'react-native-gesture-handler';
+import {AppLoading} from 'expo';
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 
-import Routes from '.src/router';
+import Routes from './src/router';
 
 
 export default function App() {
-  // o nome atribuido será usado futuramente para navegar entre as as telas, será inserido em  onPress={() => navigation.navigate('nomedatela)
   return(
     <>
       <StatusBar style="auto" backgroundColor="#000" translucent={true}/>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Routes from '.src/router';
 
 
 import Home from './pages/Home';
@@ -12,19 +11,18 @@ import Cadastro from './pages/Cadastro';
 
 const Stack = createStackNavigator();
 
+ // o nome atribuido será usado futuramente para navegar entre as as telas, será inserido em  onPress={() => navigation.navigate('nomedatela)
 
-function Routes(){
+ export default function Routes(){
     return (
         <NavigationContainer>
           <Stack.Navigator initialRouteName="home">
-             <Stack.Screen name="home" component={Home} options={{headerShown: false}} />
-             <Stack.Screen name="carrinho" component={Carrinho} options={{headerShown: false}}/>
-             <Stack.Screen name="perfil" component={Perfil} options={{headerShown: false}} />
-             <Stack.Screen name="detail" component={Detail} options={{headerShown: false}} />
-             <Stack.Screen name="cadastro" component={Cadastro} options={{headerShown: false}} />
+             <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
+             <Stack.Screen name="Carrinho" component={Carrinho} options={{headerShown: false}}/>
+             <Stack.Screen name="Perfil" component={Perfil} options={{headerShown: false}} />
+             <Stack.Screen name="Detail" component={Detail} options={{headerShown: false}} />
+             <Stack.Screen name="Cadastro" component={Cadastro} options={{headerShown: false}} />
           </Stack.Navigator>
         </NavigationContainer>
       ); 
 }
-
-export default Routes;
