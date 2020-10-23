9a53888df5476a25a913ac2fe7e7b650b50a9910
rafael-conrado <rafael070893@hotmail.com>
2020-10-22 23:13:14 -0300

    pagina teste

---
+++ src/pages/Teste/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default function Teste() {
+ return (
+
+    var person={"first_name":"Tony","last_name":"Hawk","age":31};
+    var personJSONString=JSON.stringify(person); 
+    
+    const myObjStr = JSON.stringify(myObj);
+
+console.log(myObjStr);
+// "{"name":"Skip","age":2,"favoriteFood":"Steak"}"
+
+console.log(JSON.parse(myObjStr));
+// Object {name:"Skip",age:2,favoriteFood:"Steak"}
+
+   <View>
+
+   </View>
+  );
+}
