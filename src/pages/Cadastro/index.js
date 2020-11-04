73f73f1fb3690dbc8d389d138932d6c7aa516359
Programador <programador@tline.int.br>
2020-11-04 09:14:15 -0300

    consumindo api

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,18 +1,17 @@
-import * as React from 'react';
-import { View,Button } from 'react-native';
+import React from 'react';
+import { View,Button, TextInput } from 'react-native';
 import styles from './styles'
-import InputOutline from 'react-native-input-outline';
 
 
 export default function Cadastro({ navigation}) {
   return (
     <View style={styles.container} >
-      <InputOutline  placeholder="Digite seu nome completo"  focusedColor='blue' defaultColor='grey'/>
-      <InputOutline placeholder="Digite seu CPF" focusedColor='blue' defaultColor='grey' />
-      <InputOutline placeholder="Digite seu e-mail nome@provedor.com" focusedColor='blue' defaultColor='grey'/>
-      <InputOutline  placeholder="Digite sua data de nascimento **/**/****" focusedColor='blue' defaultColor='grey'/>
-      <InputOutline placeholder="Digite sua senha" focusedColor='blue' defaultColor='grey' />
-       <InputOutline placeholder="Repita sua senha" focusedColor='blue' defaultColor='grey'/>
+      <TextInput  placeholder="Digite seu nome completo"  focusedColor='blue' defaultColor='grey'/>
+      <TextInput placeholder="Digite seu CPF" focusedColor='blue' defaultColor='grey' />
+      <TextInput placeholder="Digite seu e-mail nome@provedor.com" focusedColor='blue' defaultColor='grey'/>
+      <TextInput  placeholder="Digite sua data de nascimento **/**/****" focusedColor='blue' defaultColor='grey'/>
+      <TextInput placeholder="Digite sua senha" focusedColor='blue' defaultColor='grey' />
+       <TextInput placeholder="Repita sua senha" focusedColor='blue' defaultColor='grey'/>
       <Button title='Cadastrar' onPress={() => navigation.navigate('Home')} />
       <Button title='Limpar' onPress={() => navigation.navigate('')} />
     </View>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from '../../pages/Home';
-import Detail from '../../pages/Detail';
-import Carrinho from '../../pages/Carrinho';
-import Perfil from '../../pages/Perfil';
-import Cadastro from '../../pages/Cadastro';
-import Vitrine from '../../pages/Vitrine';
+import Home from '../pages/Home';
+import Detail from '../pages/Detail';
+import Carrinho from '../component/Carrinho';
+import Perfil from '../pages/Perfil';
+import Cadastro from '../pages/Cadastro';
+import Vitrine from '../component/Vitrine';
 
 const Stack = createStackNavigator();
 
@@ -15,15 +15,15 @@ const Stack = createStackNavigator();
 
 export default function Routes() {
   return (
-    
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Detail">
+        <Stack.Navigator initialRouteName="Vitrine">
           <Stack.Screen name="Perfil" component={Perfil} options={{ headerShown: false }} />
           <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
           <Stack.Screen name="Carrinho" component={Carrinho} options={{ headerShown: false }} />
           <Stack.Screen name="Detail" component={Detail} options={{ headerShown: false }} />
           <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
           <Stack.Screen name="Vitrine" component={Vitrine} options={{ headerShown: false }} />
+
         </Stack.Navigator>
       </NavigationContainer>
     
