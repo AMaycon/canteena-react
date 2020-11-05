23a6e0595d1fecb6ff320b7104b900142a689197
Programador <programador@tline.int.br>
2020-11-05 11:25:34 -0300

    correcao api

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-import axios from 'axios'
-
-export default axios.create({
-    baseURL: "http://192.168.23.2:5000/api"
+import api from 'apisauce'
+//na base url fica o endereço da api, caso esteja rodando local deve color o ipv4
+export default  api.create({
+    baseURL: "http://192.168.11.150:5000/api"
 })
